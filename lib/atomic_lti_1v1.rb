4f53995d2ec020ca518f1e3adad12b391e97218d
Nick Benoit <nick.benoit@atomicjolt.com>
2022-05-11 08:36:27 -0600

    Add secret_provider

--- lib/atomic_lti_1v1.rb
+++ lib/atomic_lti_1v1.rb
@@ -3,5 +3,9 @@ require 'atomic_lti_1v1/engine'
 require 'atomic_lti_1v1/lti_1v1_middleware'
 
 module AtomicLti1v1
+  mattr_accessor :secret_provider
+
+  class LtiValidationFailed < StandardError; end
+
   # Your code goes here...
 end
