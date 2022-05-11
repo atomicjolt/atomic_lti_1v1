01ae04bc17a3a0160624d711b4fd5c2062ac3fcd
Nick Benoit <nick.benoit@atomicjolt.com>
2022-05-11 11:07:09 -0600

    Add rspec version test

--- spec/atomic_lti_1v1_spec.rb
+++ spec/atomic_lti_1v1_spec.rb
@@ -2,45 +2,38 @@ require 'rails_helper'
 
 
 describe AtomicLti1v1 do 
-
-  # it "has version" do
-  # end
+  it "has version" do
+    expect(AtomicLti1v1::VERSION.present?).to eq true
+  end
 
 describe AtomicLti1v1::Lti1v1Middleware do
+
+  # Mock env to pass in middleware
+  let(:env) do 
+    Rack::MockRequest::env_for(
+      "http://localhost/blog_posts", 
+      :method => "POST", 
+      :params => {
+        oauth_consumer_key: "test_oauth_consumer_key", 
+      }
+    )
+
+  end
+
+  # dummy app to validate success
+  let(:app) { ->(env) { [200, {}, "success"] } }
+ 
+  subject { AtomicLti1v1::Lti1v1Middleware.new(app) }
+  
+  it "TODO" do
+    status, _headers, _response = subject.call(env)
+    expect(status).to eq(200)
+  end
+  
   before do
-    # @user = FactoryBot.create(:user)
   end
 
-  it "test" do
-    # token = AuthToken.issue_token({ user_id: @user.id })
-    # decoded = AuthToken.valid?(token)
-    # decoded_token = decoded[0]
-    # expect(decoded_token["user_id"]).to eq @user.id
-    expect(false).to eq true
-  end
+
 end
 
 end
-
-
-# class AtomicLti1v1Test < ActiveSupport::TestCase
-#   test 'it has a version number' do
-#     assert AtomicLti1v1::VERSION
-#   end
-
-
-#   test 'test' do 
-#     let(:env) { Rack::MockRequest.env_for }
-
-#     # dummy app to validate success
-#     let(:app) { ->(env) { [200, {}, "success"] } }
-#     subject { 
-#       byebug
-#       #.new(app) 
-#     }
-
-
-#   end
-
-
-# end
