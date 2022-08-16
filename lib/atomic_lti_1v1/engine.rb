d7150c6f6fe26f37a99a71e7eeb4c7c59a077016
Nick Benoit <nick.benoit@atomicjolt.com>
2022-08-16 16:48:39 -0600

    Dont register middleware in engine

--- lib/atomic_lti_1v1/engine.rb
+++ lib/atomic_lti_1v1/engine.rb
@@ -1,11 +1,5 @@
 module AtomicLti1v1
   class Engine < ::Rails::Engine
     isolate_namespace AtomicLti1v1
-    initializer "atomic_lti.middleware" do |app|
-      app.config.app_middleware.insert_before Warden::Manager, AtomicTenant::CurrentApplicationInstanceMiddleware
-      app.config.app_middleware.insert_before AtomicTenant::CurrentApplicationInstanceMiddleware, AtomicLti1v1::Lti1v1Middleware
-    end
-
-    # middleware.use  AtomicLti1v1::Lti1v1Middleware
   end
 end
